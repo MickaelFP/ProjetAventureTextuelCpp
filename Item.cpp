1b11c911aeafcab4c5ec27bd9c0d328493e79d27
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2021-03-12 15:59:44 +0100

    commit

---
+++ Item.cpp
@@ -0,0 +1,42 @@
+#include "Item.h"
+
+Item::Item(string t, int v)
+{
+    type = "Default";
+    value = 0;
+}
+
+Item::~Item()
+{
+}
+
+bool heal
+bool value
+string type = "heal"
+
+Item (string t,)
+
+void Item::Item(Personnage &cible)
+{
+    if(type == "heal")
+    {
+        cible.setHealth(cible.getHealth()+value);
+    }
+    if(type == "damage")
+    {
+        cible.setHealth(cible.getHealth()-value);
+    }
+    if(type == "buff")
+    {
+        cible.setAttack(cible.getAttack()+value);
+    }
+}
+
+void Item::showInfo()
+{
+    cout<<"type = "<<gettype<<endl;
+}
+
+/*
+
+*/
---
+++ Item.h
@@ -0,0 +1,15 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+class Item
+{
+    private:
+    public:
+        Item();
+        virtual ~Item();
+
+        void useItem();
+        void showInfo();
+};
+
+#endif // ITEM_H
